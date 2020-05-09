8364fdad28116b21630a0b3cb2b853fb72bddd09
Daniell Algar <daniell.algar@gmail.com>
2020-05-09 19:41:37 +0200

    Style five day item

--- data/model/src/main/java/com/algar/model/Weather.kt
+++ data/model/src/main/java/com/algar/model/Weather.kt
@@ -1,8 +1,14 @@
 package com.algar.model
 
+import android.annotation.SuppressLint
+
 data class Weather(
     val id: Int,
     val main: String,
     val description: String,
     val icon: String
-)
+) {
+    val capitalizedDescription: String
+        @SuppressLint("DefaultLocale")
+        get() = description.capitalize()
+}
--- features/details/src/main/java/com/algar/details/DetailsBinding.kt
+++ features/details/src/main/java/com/algar/details/DetailsBinding.kt
@@ -1,10 +1,16 @@
 package com.algar.details
 
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.algar.model.FiveDayDetails
 import com.algar.model.FiveDayForecast
+import com.algar.model.Secrets
+import com.algar.model.prettyDate
 import com.algar.repository.utils.Resource
+import com.bumptech.glide.Glide
+import kotlin.math.roundToInt
 
 object DetailsBinding {
 
@@ -13,6 +19,33 @@ object DetailsBinding {
     fun setItems(recyclerView: RecyclerView, resource: Resource<FiveDayForecast>?) {
         with(recyclerView.adapter as DetailsAdapter) {
             update(items = resource)
+        }
+    }
+
+    @BindingAdapter("app:date")
+    @JvmStatic
+    fun setDate(view: TextView, forecast: FiveDayDetails) {
+        view.text = forecast.prettyDate()
+    }
+
+    @BindingAdapter("app:loadImage")
+    @JvmStatic
+    fun loadImage(view: ImageView, code: String?) {
+        val imageUrl = Secrets.iconUrl + code + "@2x.png"
+        Glide.with(view.context)
+            .load(imageUrl)
+            .into(view)
+    }
+
+    @BindingAdapter("android:text")
+    @JvmStatic
+    fun setFloat(view: TextView, value: Float?) {
+        view.text = if (value == null || value.isNaN()) {
+            "-"
+        } else {
+            val celsiusSymbol = "\u2103"
+            val tempWithoutDecimal = value.roundToInt()
+            "$tempWithoutDecimal$celsiusSymbol"
         }
     }
 }
